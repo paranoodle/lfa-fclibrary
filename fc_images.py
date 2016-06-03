6e64fa30f0f396b7c59cb7dcdb167c9aa18d824d
paranoodle <paranoodle@users.noreply.github.com>
2016-06-03 15:10:36 +0200

    implemented script to test k-means on images

---
+++ fc_images.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy import misc
+import fc_lib_kmean as fc
+
+
+im = misc.imread("test.png")
+print "loaded image"
+
+rows, cols = im.shape[:2]
+
+test = []
+for row in xrange(rows):
+    for col in xrange(cols):
+        test.append(fc.Point(im[row,col]))
+print "converted to points"
+
+centers = fc.kmpp(test, 8)
+print "calculated centers with k-means++"
+
+results = fc.simple_K(test, centers)
+print "calculated clusters with k-means"
+
+test2 = np.array(test).reshape(rows, cols)
+
+for row in xrange(rows):
+    for col in xrange(cols):
+        im[row,col] = test2[row,col].cluster.center
+print "finished outputting image"
+
+misc.imsave("result.png", im)
